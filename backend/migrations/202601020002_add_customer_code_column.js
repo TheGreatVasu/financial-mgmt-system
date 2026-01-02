8c9778a6063b3211f51c9c28eaa425e377e3256a
TheGreatVasu <vasurastogi213@gmail.com>
2026-01-02 18:51:05 +0530

    updates 2.02

---
+++ backend/migrations/202601020002_add_customer_code_column.js
@@ -0,0 +1,12 @@
+/**
+ * MIGRATION STUB: This file was replaced by 202601020003_add_all_missing_customers_columns.js
+ * Kept for knex migration tracking compatibility.
+ */
+
+exports.up = async function (knex) {
+  console.log('Skipping stub migration - columns added in 202601020003_add_all_missing_customers_columns.js');
+};
+
+exports.down = async function (knex) {
+  console.log('Skipping stub migration rollback');
+};
